f9aec3da238b8f5c0a0b7df9a60ed3f3ebc4543c
Bruno Lima <lima.bruno@gov.com.br>
2022-06-25 09:27:53 -0300

    Primeiro commit

---
+++ src/main/java/br/com/fattoria/FattoriaBrunoLimaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fattoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FattoriaBrunoLimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FattoriaBrunoLimaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fattoria/FattoriaBrunoLimaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fattoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FattoriaBrunoLimaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
